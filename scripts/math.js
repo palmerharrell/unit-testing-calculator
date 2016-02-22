1f4be51c52f45bb7b09a5e40685255b5bcf612da
Palmer Harrell <palmerharrell@me.com>
2016-02-22 12:11:23 -0600

    Finished unit tests. Started user interface

--- scripts/math.js
+++ scripts/math.js
@@ -3,3 +3,23 @@
 function add(num1, num2) {
 	return num1 + num2;
 }
+
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+function divide(num1, num2) {
+	return num1 / num2;
+}
+
+function square(num) {
+	return Math.pow(num, 2);
+}
+
+function squareRoot(num) {
+	return Math.sqrt(num);
+}
